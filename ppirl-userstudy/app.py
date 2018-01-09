8cb696451cd6d2cfc038a1c3c6296ea0674b2224
Qinbo <sysulqb@gmail.com>
2018-01-09 17:36:41 -0600

    heroku 500 error debuging

--- ppirl-userstudy/app.py
+++ ppirl-userstudy/app.py
@@ -242,16 +242,20 @@ def open_cell():
     id1 = request.args.get('id1')
     id2 = request.args.get('id2')
     mode = request.args.get('mode')
+    print("needle 245")
 
     pair_num = str(id1.split('-')[0])
     attr_num = str(id1.split('-')[2])
+    print("needle 249")
 
     pair = dl.get_pair('data/ppirl.csv', pair_num)
     record1 = pair[0]
     record2 = pair[1]
+    print("needle 254")
 
     attr1 = record1[int(attr_num)]
     attr2 = record2[int(attr_num)]
+    print("needle 258")
 
     if attr_num == '1':
         # id
@@ -283,10 +287,12 @@ def open_cell():
         helper1 = record1[14]
         helper2 = record2[14]
         get_display = dd.get_character_display
+    print("needle 290")
 
     attr_full = get_display(attr1=attr1, attr2=attr2, helper1=helper1, helper2=helper2, attribute_mode='full')
     attr_partial = get_display(attr1=attr1, attr2=attr2, helper1=helper1, helper2=helper2, attribute_mode='partial')
     attr_masked = get_display(attr1=attr1, attr2=attr2, helper1=helper1, helper2=helper2, attribute_mode='masked')
+    print("needle 295")
 
     ret = dict()
     if mode == 'masked':
@@ -296,6 +302,7 @@ def open_cell():
             ret = {"value1": attr_partial[0], "value2": attr_partial[1], "mode": "partial"}
     elif mode == 'partial':
         ret = {"value1": attr_full[0], "value2": attr_full[1], "mode": "full"}
+    print("needle 305")
 
     # get character disclosed percentage
     cdp_previous_attr1 = 0
@@ -303,6 +310,7 @@ def open_cell():
     if mode == 'partial':
         cdp_previous_attr1 = dd.get_character_disclosed_num(helper1)
         cdp_previous_attr2 = dd.get_character_disclosed_num(helper2)
+    print("needle 313")
 
     cdp_post_attr1 = len(attr1)
     cdp_post_attr2 = len(attr2)
@@ -313,6 +321,7 @@ def open_cell():
     session_data[session['user_cookie']]['disclosed_chrarcters'] += cdp_increment
     cdp = 100.0*session_data[session['user_cookie']]['disclosed_chrarcters']/session_data[session['user_cookie']]['total_characters']
     ret['cdp'] = cdp
+    print("needle 324")
 
     print("returning: ")
     print(ret)
