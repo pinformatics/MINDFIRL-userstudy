3301413f2fa854efaf3d0f47744a2c3711ac52be
qinboli <leoli@easyops.cn>
2018-03-03 16:18:30 -0600

    merge 2

--- ppirl-userstudy/app.py
+++ ppirl-userstudy/app.py
@@ -577,21 +577,6 @@ def show_section2_guide():
     # grading section 1
     user_data_key = session['user_cookie'] + '_user_data'
     user_data = r.get(user_data_key)
-<<<<<<< HEAD
-
-    if r.get("data_choice_" + session['user_cookie']) == "collect": 
-        # print "collcted"
-        data = ud.parse_user_data(user_data)
-        result = ud.grade_final_answer(data, DATA_PAIR_LIST)
-        performance1 = 'type:performance1,content:' + str(result[0]) + ' out of ' + str(result[1]) + ';\n'
-        user_data += performance1
-
-        result2 = ud.grade_final_answer(data, DATA_SECTION2)
-        performance2 = 'type:performance2,content:' + str(result2[0]) + ' out of ' + str(result2[1]) + ';\n'
-        user_data += performance2
-
-        r.set(user_data_key, user_data)
-=======
     data = ud.parse_user_data(user_data)
     result = ud.grade_final_answer(data, DATA_PAIR_LIST)
     performance1 = 'type:performance1,content:' + str(result[0]) + ' out of ' + str(result[1]) + ';\n'
