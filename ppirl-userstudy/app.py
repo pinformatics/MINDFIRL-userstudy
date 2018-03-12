3f33b0cf7a1dc1f1bebd5f9787369df549977571
qinboli <sysulqb@gmail.com>
2018-03-12 12:45:58 -0500

    debug

--- ppirl-userstudy/app.py
+++ ppirl-userstudy/app.py
@@ -15,8 +15,8 @@ import data_display as dd
 import data_model as dm
 import user_data as ud
 import config
-from main_section import main_section
-from tutorial import tutorial
+#from main_section import main_section
+#from tutorial import tutorial
 from util import state_machine
 from global_data import *
 
